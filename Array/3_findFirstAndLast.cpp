9ff40d2c523b36587cb99c94b1206d5ed26c9bc3
Kashif Ahmad <89984617+KashifAhmad1789@users.noreply.github.com>
2021-09-02 20:30:37 +0530

    First And Last Occurance Of Array

---
+++ Array/3_findFirstAndLast.cpp
@@ -0,0 +1,27 @@
+/*Find first and last positions of an element in a sorted array
+Difficulty Level : Easy
+Last Updated : 12 Aug, 2021
+Given a sorted array with possibly duplicate elements, the task is to find indexes of first and last occurrences of an element x in the given array. 
+
+Examples: 
+
+Input : arr[] = {1, 3, 5, 5, 5, 5, 67, 123, 125}    
+        x = 5
+Output : First Occurrence = 2
+         Last Occurrence = 5
+
+Input : arr[] = {1, 3, 5, 5, 5, 5, 7, 123, 125 }    
+        x = 7
+Output : First Occurrence = 6
+         Last Occurrence = 6
+         */
+
+        
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   
+   return 0;
+}
